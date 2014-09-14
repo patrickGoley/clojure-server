f6788a6648beea9c5c63a23283d7c0c31604f35d
Patrick Goley <patrick.goley@gmail.com>
2014-09-14 17:31:03 -0500

    updated lobos config, defined migrations

--- src/lobos/config.clj
+++ src/lobos/config.clj
@@ -1,12 +1,8 @@
-(ns lobos.config
-  (:use [lobos.connectivity]))
+(ns lobos.config)
 
 (def db
   {:classname "org.postgresql.Driver"
    :subprotocol "postgresql"
    :user "cgnzeetnpkoqyc"
    :password "-eABQ0ah6GObSZzUY44hKtxbru"
-   :subname "//localhost:5432/px"})
-
-
-(open-global db)
+   :subname "//localhost:5432/http-server"})
--- src/lobos/migrations.clj
+++ src/lobos/migrations.clj
@@ -1,9 +1,27 @@
 (ns lobos.migrations
-  (:refer-clojure :exclude [alter defonce drop bigint boolean char double float time])
-  (:use (lobos [migration :only [defmigration]] core schema) lobos.config))
+  (:refer-clojure :exclude [alter drop
+                            bigint boolean char double float time])
+  (:use (lobos [migration :only [defmigration]] core schema config helpers)))
 
-(defmigration create-users
-  (up [] (create (table :users
-                        (integer :id :primary-key)
-                        (varchar :email 256 :unique))))
+(defmigration add-users-table
+  (up [] (create
+          (tbl :users
+               (varchar :name 100 :unique)
+               (check :name (> (length :name) 1)))))
   (down [] (drop (table :users))))
+
+(defmigration add-posts-table
+  (up [] (create
+          (tbl :posts
+               (varchar :title 200 :unique)
+               (text :content)
+               (refer-to :users))))
+  (down [] (drop (table :posts))))
+
+(defmigration add-comments-table
+  (up [] (create
+          (tbl :comments
+               (text :content)
+               (refer-to :users)
+               (refer-to :posts))))
+  (down [] (drop (table :comments))))
