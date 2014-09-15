4bc08f8c037a2dda6de513dfbce1efe876b19313
Patrick Goley <patrick.goley@gmail.com>
2014-09-14 23:17:19 -0500

    updated lobos config to work on heroku

--- src/lobos/config.clj
+++ src/lobos/config.clj
@@ -1,8 +1,39 @@
-(ns lobos.config)
+(ns lobos.config
+  (:require [clojure.string :as str] lobos.connectivity)
+  (:import (java.net URI)))
+
+(defn heroku-db
+  "Generate the dp map according to the Heroku environment when available."
+  []
+  (when (System/getenv "DATABASE_URL")
+    (let [url (URI. (System/getenv "DATABASE_URL"))
+          host (.getHost url)
+          port (if (pos? (.getPort url)) (.getPort url) 5432)
+          path (.getPath url)]
+      (merge
+       {:subname (str "//" host ":" port path)}
+       (when-let [user-info (.getUserInfo url)]
+         {:user (first (str/split user-info #":"))
+          :password (second (str/split user-info #":"))})))))
+
+(defn open-global-when-necessary
+  "Open a global connection only when necessary, that is, when no previous
+  connection exists or when dp-spec if different to the current global
+  connection."
+  [db-spec]
+  ;; If the connection credentials have changed, close the connection.
+  (when (and (@lobos.connectivity/global-connections :default-connection)
+             (not= (:db-spec (@lobos.connectivity/global-connections :default-connection)) db-spec))
+    (lobos.connectivity/close-global))
+  ;; Open a new connection or return the existing connection.
+  (if (nil? (@lobos.connectivity/global-connections :default-connection))
+    ((lobos.connectivity/open-global db-spec) :default-connection)
+    (@lobos.connectivity/global-connections :default-connection)))
 
 (def db
-  {:classname "org.postgresql.Driver"
-   :subprotocol "postgresql"
-   :user "cgnzeetnpkoqyc"
-   :password "-eABQ0ah6GObSZzUY44hKtxbru"
-   :subname "//localhost:5432/http-server"})
+  (merge {:classname "org.postgresql.Driver"
+          :subprotocol "postgresql"
+          :subname "//localhost:5432/http-server"}
+         (heroku-db)))
+
+(open-global-when-necessary db)
